f3526b95ba01faf6ef2377bb5e3f0499813820bc
Zedination <50323429+Zedination@users.noreply.github.com>
2020-05-22 01:20:13 +0700

    fix bug

--- src/main/java/com/example/restcontroller/BaseRest.java
+++ src/main/java/com/example/restcontroller/BaseRest.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BaseRest {
-	@GetMapping(path = {"/","/webhook"})
+	@GetMapping("/")
 	public String welcome() {
 		return "Welcome to my first webhook!";
 	}
