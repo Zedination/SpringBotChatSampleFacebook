522ee48c9cc83832d2aac5b22614315d132e7618
Zedination <50323429+Zedination@users.noreply.github.com>
2021-07-06 16:10:58 +0700

    fix bug rest template

--- src/main/java/com/example/SpringBotChatSampleFacebookApplication.java
+++ src/main/java/com/example/SpringBotChatSampleFacebookApplication.java
@@ -2,8 +2,6 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot", "com.example"})
 public class SpringBotChatSampleFacebookApplication {
@@ -12,8 +10,8 @@ public class SpringBotChatSampleFacebookApplication {
 		SpringApplication.run(SpringBotChatSampleFacebookApplication.class, args);
 	}
 	
-	@Bean
-	public RestTemplate restTemplate() {
-	    return new RestTemplate();
-	}
+//	@Bean
+//	public RestTemplate restTemplate() {
+//	    return new RestTemplate();
+//	}
 }
